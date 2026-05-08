101df416bf27652bf4f50f80446bf596b7824564
thiagolimadyo <thiagolimadyo@outlook.com>
2026-05-08 07:04:41 -0300

    teste2

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
+#![allow(unused)]
 fn main() {
     let mut x1 = 10;
-    let x2;
+    let mut x2;
     x1 = x1 * 10;
     x2 = x1 - 2;
+    x2 = 10;
     println!("x1 is: {}, x2 is: {}", x1, x2);
 }
